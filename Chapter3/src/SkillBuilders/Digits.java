cd6cb435124e4ea36ee3bbcaf7ef5b0432073eb3
39046001 <39046001@L815WD220018.edu.cbe.ab.ca>
2024-09-18 11:48:17 -0600

    11:48

---
+++ Chapter3/src/SkillBuilders/Digits.java
@@ -0,0 +1,18 @@
+package SkillBuilders;
+
+import java.util.Scanner; // Import scanner
+
+public class Digits {
+
+	public static void main(String[] args) {
+		Scanner Userinput = new Scanner (System.in);
+System.out.print("Enter a two digit number: ");
+int digitnumber = Userinput.nextInt();
+
+int tens = digitnumber / 10;
+int ones = digitnumber % 10;
+System.out.println("In the tens place of the number " + digitnumber + " is: " + tens);
+System.out.println("In the ones place of the number " + digitnumber + " is: " + ones);
+}
+
+}
