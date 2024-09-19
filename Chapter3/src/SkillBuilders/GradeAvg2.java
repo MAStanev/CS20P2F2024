c9edce279185e9490e44fd89d84269b8125ba8cf
39046001 <39046001@L815WD220018.edu.cbe.ab.ca>
2024-09-19 09:47:18 -0600

    9:47 AM

---
+++ Chapter3/src/SkillBuilders/GradeAvg2.java
@@ -0,0 +1,28 @@
+package SkillBuilders;
+
+import java.util.Scanner; // Import scanner
+
+public class GradeAvg2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub		
+		
+
+Scanner Userinput = new Scanner (System.in);
+
+
+System.out.println("Please enter five grades.");
+System.out.println("Enter your first grade: ");
+int grade1 = Userinput.nextInt();
+System.out.println("Enter your second grade: ");
+int grade2 = Userinput.nextInt();
+System.out.println("Enter your third grade: ");
+int grade3 = Userinput.nextInt();
+System.out.println("Enter your fourth grade: ");
+int grade4 = Userinput.nextInt();
+System.out.println("Enter your fifth grade: ");
+int grade5 = Userinput.nextInt();
+int total = (grade1 + grade2 + grade3 + grade4 + grade5);
+double gradeavg = (double) total / 5;
+ System.out.print("Your average grade is " + gradeavg);
+
