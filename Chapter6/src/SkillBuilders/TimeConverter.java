1d12308e4752ffccb166f02f40cf03abbc751a0f
39046001 <39046001@L815WD220018.edu.cbe.ab.ca>
2024-10-29 09:21:10 -0600

    9:20 AM

---
+++ Chapter6/src/SkillBuilders/TimeConverter.java
@@ -0,0 +1,15 @@
+package SkillBuilders;
+
+import java.util.Scanner;
+
+public class TimeConverter {
+
+	public static void main(String[] args) {
+
+Scanner userInput = new Scanner(System.in);
+        
+System.out.println("Choose which time you would like to convert between");
+System.out.print("1. Hours to minutes");
+	}
+
+}
