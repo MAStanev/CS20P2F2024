9120882ce74312d6f0ec589f497b5b5ea45e64cc
39046001 <39046001@L815WD220018.edu.cbe.ab.ca>
2024-11-26 10:23:17 -0700

    10:23 AM

--- Chapter7/src/Masterys/MySavings.java
+++ Chapter7/src/Masterys/MySavings.java
@@ -3,128 +3,126 @@ package Masterys;
 import java.util.*;
 import java.text.DecimalFormat;
 
-public class MySavings {	
+public class MySavings {
 
-	private double total;
+    private double total;
+    private int penny, nickel, dime, quarter;
 
-	private int penny, nickel, dime, quarter;
+    public MySavings() {
+        total = 0; // Default amount of money
+    }
 
-	public MySavings() {
+    public MySavings(int penny, int nickel, int dime, int quarter) {
+        total = (penny * 0.01) + (nickel * 0.05) + (dime * 0.1) + (quarter * 0.25); // Value of each coin
+    }
 
-		total = 0; //Default amount of money
-}
+    public void setTotal() {
+        total = penny * 0.01 + nickel * 0.05 + dime * 0.1 + quarter * 0.25;
+    }
 
-public MySavings(int penny, int nickel, int dime, int quarter) 
-{
-	total = (penny * 0.01) + (nickel * 0.05) + (dime * 0.1) +(quarter * 0.25); // Value of each coin
-}
-public void setTotal() 
-{
-	total = penny*0.01+nickel*0.05+dime*0.1+quarter*0.25;
-}
-public void setPenny(int p)
-{
-	penny += p;
-}
-public void setNickel(int n)
-{
-	nickel += n;
-}
-public void setDime(int d) 
-{
-	dime += d;
-}
-public void setQuarter(int q) 
-{
-	quarter += q;
-}
-public double getTotal()
-{
-	return total;
-}
-public boolean TakeMoney(double TOMoney) 
-{
-	if (TOMoney<=total) {
-		total-=TOMoney;
-		return true;
-}
+    public void setPenny(int p) {
+        penny += p;
+    }
 
-else 
-{
-return false;
-}
+    public void setNickel(int n) {
+        nickel += n;
+    }
 
-}
+    public void setDime(int d) {
+        dime += d;
+    }
 
-public static void main(String[] args) 
-{
-Scanner userinput = new Scanner (System.in);
+    public void setQuarter(int q) {
+        quarter += q;
+    }
 
-MySavings PiggyBank = new MySavings();
+    public double getTotal() {
+        return total;
+    }
 
-DecimalFormat decimalFormat = new DecimalFormat("$0.00");
+    public boolean TakeMoney(double TOMoney) {
+        if (TOMoney <= total) {
+            total -= TOMoney;
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-int userChoice;
+    public static void main(String[] args) {
+        Scanner userinput = new Scanner(System.in);
+        MySavings PiggyBank = new MySavings();
+        DecimalFormat decimalFormat = new DecimalFormat("$0.00");
 
-do {
-System.out.println("1. Show total in bank."); // Prompt the user with options
-System.out.println("2. Add pennies.");
-System.out.println("3. Add nickels.");
-System.out.println("4. Add dimes.");
-System.out.println("5. Add quarters.");
-System.out.println("6. Take money out of bank.");
-System.out.println("Enter 0 to quit.");
-System.out.print("Enter your choice: ");
-userChoice = userinput.nextInt(); // Store input
+        int userChoice;
 
-switch (userChoice) { // Switch case statement which prints something depending on userChoice
-case 1:
-PiggyBank.setTotal();
-System.out.println("Bank balance: "+ decimalFormat.format(PiggyBank.getTotal()));
-System.out.println(" ");
-break;
+        do {
+            System.out.println("1. Show bank balance.");
+            System.out.println("2. Add pennies.");
+            System.out.println("3. Add nickels.");
+            System.out.println("4. Add dimes.");
+            System.out.println("5. Add quarters.");
+            System.out.println("6. Withdraw money.");
+            System.out.println("Enter 0 to quit.");
+            System.out.print("Enter your choice: ");
+            userChoice = userinput.nextInt();
 
-case 2:
-System.out.print("Enter the amount of pennies: ");
-PiggyBank.setPenny(userinput.nextInt());
-System.out.println(" ");
-break;
+            switch (userChoice) {
+                case 1: {
+                    PiggyBank.setTotal();
+                    System.out.println("Bank balance: " + decimalFormat.format(PiggyBank.getTotal()));
+                    System.out.println(" ");
+                    break;
+                }
 
-case 3:
-System.out.print("Enter the amount of nickels: ");
-PiggyBank.setNickel(userinput.nextInt());
-System.out.println(" ");
-break;
+                case 2: {
+                    System.out.print("Enter amount of pennies: ");
+                    PiggyBank.setPenny(userinput.nextInt());
+                    System.out.println(" ");
+                    break;
+                }
 
-case 4:
-System.out.print("Enter the amount of dimes: ");
-PiggyBank.setDime(userinput.nextInt());
-System.out.println(" ");
-break;
+                case 3: {
+                    System.out.print("Enter amount of nickels: ");
+                    PiggyBank.setNickel(userinput.nextInt());
+                    System.out.println(" ");
+                    break;
+                }
 
-case 5:
-System.out.print("Enter the amount of quarters: ");
-PiggyBank.setQuarter(userinput.nextInt());
-System.out.println(" ");
-break;
+                case 4: {
+                    System.out.print("Enter amount of dimes: ");
+                    PiggyBank.setDime(userinput.nextInt());
+                    System.out.println(" ");
+                    break;
+                }
 
-case 6:
-System.out.print("Enter number of money you want to take out: $");
-double MoneyTakeOut = userinput.nextDouble();
-if (PiggyBank.TakeMoney(MoneyTakeOut)) {
-System.out.println("Now your account remains: $"+PiggyBank.getTotal());
-}
+                case 5: {
+                    System.out.print("Enter amount of quarters: ");
+                    PiggyBank.setQuarter(userinput.nextInt());
+                    System.out.println(" ");
+                    break;
+                }
 
-else 
-{
-System.out.println("Invalid, please try again.");
-}
-System.out.println(" ");
-break;
-}
+                case 6: {
+                    System.out.print("Enter the amount of money you want to withdraw: $");
+                    double MoneyTakeOut = userinput.nextDouble();
+                    if (PiggyBank.TakeMoney(MoneyTakeOut)) {
+                        System.out.println("Remaining bank balance: $" + decimalFormat.format(PiggyBank.getTotal()));
+                    } else {
+                        System.out.println("Invalid input | Insufficient balance.");
+                    }
+                    System.out.println(" ");
+                    break;
+                }
 
-} while(userChoice !=0);
+                default: {
+                    System.out.println("Invalid input | Please try again.");
+                    break;
+                }
+            }
 
-System.out.print("End of Program! Thanks for using."); //End of program//
-}
+        } while (userChoice != 0);
+
+        System.out.println("Thank you for using."); // End of program
+    }
 }
